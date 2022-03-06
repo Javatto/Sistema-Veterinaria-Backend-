1bd38c7c432544776a5a3b42d9e14f35903f918d
Javatt <javier.martinez.carranzaipn.gmail>
2022-03-06 16:01:32 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql');
+const myconn = require('express-myconnection');
+const routesCliente = require('./routes/routesCliente.js');
+const routesMascota = require('./routes/routesMascota.js');
+
+const app = express();
+app.set('port', process.env.PORT || 9000);
+app.use(express.json());
+
+app.listen(app.get('port'),() => {
+    console.log('server running on port',app.get('port'));
+});
+
+//================ conexion BD ============
+const dbOptions = {
+    port: 3306,
+    host: 'localhost',
+    user: 'admin',
+    password: '123',
+    database: 'sistema veterinaria'
+}
+ 
+app.use(myconn(mysql,dbOptions,'single'));
+
+//=================== routes ==============
+app.get('/', (request, response) => {
+    response.send('Servidor en marcha, sistema veterinaria');
+}); ``
+
+app.use('/api/cliente',routesCliente);
+app.use('/api/mascota',routesMascota);
+
+
---
+++ routes/routesCliente.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const routes = express.Router();
+
+// ============ Consultar datos ========
+routes.get('/', (request,response) => {
+    request.getConnection((err,conn)=>{
+        if(err) return response.send(err);
+
+        conn.query('SELECT * FROM cliente', (err,rows) => {
+            if(err) return response.send(err);
+
+            response.json(rows);
+        });
+    });
+});
+
+// ============ Agregar datos ========
+routes.post('/', (request,response) => {
+    request.getConnection((err,conn)=>{
+        if(err) return response.send(err);
+        conn.query('INSERT INTO cliente set ?',[request.body],(err,rows) => {
+            if(err) return response.send(err);
+
+            response.json(rows.affectedRows);
+        });
+    });
+});
+
+// ============ Eliminar datos ========
+routes.delete('/:id', (request,response) => {
+    request.getConnection((err,conn)=>{
+        if(err) return response.send(err);
+        conn.query('DELETE FROM cliente WHERE id = ?',[request.params.id],(err,rows) => {
+            if(err) return response.send(err);
+            response.json(rows);
+        });
+    });
+});
+
+// ============ Actualizar datos ========
+routes.put('/:id', (request,response) => {
+    request.getConnection((err,conn)=>{
+        if(err) return response.send(err);
+        conn.query('UPDATE cliente SET ? WHERE id = ?',[request.body,request.params.id],(err,rows) => {
+            if(err) return response.send(err);
+            response.json(rows);
+        });
+    });
+});
+
+module.exports = routes;
